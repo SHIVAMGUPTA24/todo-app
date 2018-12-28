e8b4b2d4f5ec6913bd3755915ca53bf2f3918337
SHIVAMGUPTA24 <shivam20292414@gmail.com>
2018-12-28 23:31:39 +0530

    basics

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var todocontroller = require('./controllers/todoController');
+var app=express();
+app.set('view engine','ejs');
+app.use(express.static('./public'));
+todocontroller(app);
+
+
+app.listen(3053);
+console.log("u r listing to port 3054");
