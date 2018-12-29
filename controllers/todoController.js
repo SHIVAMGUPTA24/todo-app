6623cdde130b9c754cdbf040d671c8a2b897293d
SHIVAMGUPTA24 <shivam20292414@gmail.com>
2018-12-29 21:23:35 +0530

    final commit

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,21 +1,48 @@
+var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-var data=[{item:'getmilk'},{item:'milk'},{item:'sexy'}];
+mongoose.connect('mongodb://shivam:shivam24@ds145434.mlab.com:45434/todo');
+
+var todoSchema = new mongoose.Schema({
+  item:String
+
+});
+var Todo=mongoose.model('Todo',todoSchema);
+// var itemOne=Todo({item:'buy flowers'}).save(function (err) {
+//   if(err)
+//   {
+//     throw err;
+//   }
+//   console.log('item saved');
+//
+// });
+
+// var data=[{item:'getmilk'},{item:'milk'},{item:'sexy'}];
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 module.exports=function(app){
   app.get('/todo',function(req,res){
-    res.render('todo',{todos:data});
+    Todo.find({},function(err,data){
+      if(err) throw err;
+      res.render('todo',{todos:data});
+
+
+    });
 
   });
   app.post('/todo',urlencodedParser,function(req,res){
-    data.push(req.body);
-    res.json(data);
-    });
-  app.delete('/todo/:item',function(req,res){
-    data=data.filter(function(todo){
-      return todo.item.replace(/ /g,"-")!== req.params.item;
+    var newTodo=Todo(req.body).save(function(err,data){
+      if(err) throw err;
+      res.json(data);
+
 
     });
-    res.json(data);
+    });
+  app.delete('/todo/:item',function(req,res){
+    Todo.find({item:req.params.item.replace(/'-'/g," ")}).remove(function(err,data){
+      if(err) throw err;
+      res.json(data);
+    });
+
+
 
   });
 
